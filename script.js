f6b7343b8c2e39dbcc12665befa253f2e9e2c73a
nurakbarova <nurayakbarova76@gmail.com>
2023-04-16 17:43:09 +0400

    array

---
+++ script.js
@@ -0,0 +1,73 @@
+// // // // TASK1 "C"
+// // var arr=[32,25,85,89,45,76,7,41,2,12]
+// // // arr.push(15)
+// // // console.log(arr)
+// // // // TASK1 "D"
+// // // arr.shift()
+// // // console.log(arr)
+// // // TASK1 "E"
+// // arr.sort();
+// // console.log(arr)
+
+
+// // let cars=[" bmw","lada"]
+// // cars.pop()
+// // console.log(cars)
+
+
+// // TASK1 "A"
+// var arr=[32,25,85,89,45,76,7,41,2,12]
+// console.log(arr);
+
+// // TASK1 "B"
+// var arr=[32,25,85,89,45,76,7,41,2,12]
+
+
+
+
+
+// // TASK1 "C"
+// var arr=[32,25,85,89,45,76,7,41,2,12]
+// arr.push(15)
+// console.log(arr);
+
+
+// // TASK1 "D"
+// var arr=[32,25,85,89,45,76,7,41,2,12]
+// arr.shift()
+// console.log(arr);
+
+// // TASK1 "E"
+// var arr=[32,25,85,89,45,76,7,41,2,12]
+
+
+   
+// // TASK1 "F"
+// var arr=[32,25,85,89,45,76,7,41,2,12] 
+// arr.splice(3,0,"43")
+// console.log(arr);
+
+
+   
+// // TASK1 "G"
+// var arr=[32,25,85,89,45,76,7,41,2,12] 
+// let newArr=arr.slice(3,arr.length)
+// console.log(newArr);
+
+
+
+// // TASK1 "H"
+// var arr=[32,25,85,89,45,76,7,41,2,12] 
+// arr.forEach(myFunction);
+//     function myFunction(item){
+//         console.log(item);
+//     }
+
+
+// // TASK1 "H"
+// var arr=[32,25,85,89,45,76,7,41,2,12] 
+// let newArr=arr.filter((item)=>
+// {
+//     return item>40
+// })
+// console.log(newArr);
